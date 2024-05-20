8aa4cf78fad15184ba2eb5ff24a7352d17fcf4a4
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-20 22:53:14 +0200

    feat: Add fake urls

--- src/test_detect_csv_url.py
+++ src/test_detect_csv_url.py
@@ -18,10 +18,10 @@ class TestNotCsvContentTypeReaderService(ContentTypeReaderServiceInterface):
 def test_csv_url_content_type():
     all_content_type = TestCsvContentTypeReaderService()
     content_type = CsvDective(all_content_type)
-    assert content_type.is_csv("https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/prenoms-des-enfants-nes-a-angers@angersloiremetropole/exports/csv") is True
+    assert content_type.is_csv("https://fakedata.opendatasoft.com/apii/explore/v2.1/catalog/datasets/prenoms-des-enfants-nes-a-angers@angersloiremetropole/exports/csv") is True
 
 
 def test_not_csv_url_content_type():
     all_content_type = TestNotCsvContentTypeReaderService()
     content_type = CsvDective(all_content_type)
-    assert content_type.is_csv("https://gitlab.com/validata-table/validata-table/uploads/f0091cba92766b0015fb0015e8c594fc/prenoms-des-enfants-nes-a-angers_angersloiremetropole.csv") is False
+    assert content_type.is_csv("https://fakegitlab.com/validata-table/validata-table/uploads/f0091cba92766b0015fb0015e8c594fc/prenoms-des-enfants-nes-a-angers_angersloiremetropole.csv") is False
