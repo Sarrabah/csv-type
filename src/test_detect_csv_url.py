bbe2b2c4c8fc2921a3d877219ea72b68ba9b3fd7
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-21 11:23:12 +0200

    test: Test a void content type

--- src/test_detect_csv_url.py
+++ src/test_detect_csv_url.py
@@ -1,4 +1,3 @@
-import pytest
 from detect_csv_url import CsvDective
 from url_content_type_services.url_content_type_services import ContentTypeReaderServiceInterface
 
@@ -15,6 +14,12 @@ class TestNotCsvContentTypeReaderService(ContentTypeReaderServiceInterface):
         return content_type
 
 
+class TestVoidContentTypeReaderService(ContentTypeReaderServiceInterface):
+    def get_content_type_from_url_header(self, url: str) -> str:
+        content_type = ""
+        return content_type
+
+
 def test_csv_url_content_type():
     all_content_type = TestCsvContentTypeReaderService()
     content_type = CsvDective(all_content_type)
@@ -25,3 +30,9 @@ def test_not_csv_url_content_type():
     all_content_type = TestNotCsvContentTypeReaderService()
     content_type = CsvDective(all_content_type)
     assert content_type.is_csv("https://fakegitlab.com/validata-table/validata-table/uploads/f0091cba92766b0015fb0015e8c594fc/prenoms-des-enfants-nes-a-angers_angersloiremetropole.csv") is False
+
+
+def test_void_url_content_type():
+    all_content_type = TestVoidContentTypeReaderService()
+    content_type = CsvDective(all_content_type)
+    assert content_type.is_csv("https://fakedata.opendatasoft.com/apii/explore/v2.1/catalog/datasets/prenoms-des-enfants-nes-a-angers@angersloiremetropole/exports/csv") is False
