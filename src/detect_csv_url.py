d5434cee0e95bd152090a95af7145a2a61044408
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-21 11:22:19 +0200

    refactor: Linting

--- src/detect_csv_url.py
+++ src/detect_csv_url.py
@@ -4,7 +4,7 @@ from url_content_type_services.url_content_type_services import ContentTypeReade
 class CsvDective:
     def __init__(self, content_type: ContentTypeReaderServiceInterface):
         self.content_type = content_type
-    
+
     def is_csv(self, url: str) -> bool:
         content_type = self.content_type.get_content_type_from_url_header(url)
         content_type_arr = content_type.split(";")
