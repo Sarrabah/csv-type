e3df93772fcd0c408556f0a1f716f2204d5f7ec9
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-22 14:53:39 +0200

    refactor: Delete the class, constructor and add another condition

--- src/detect_csv_url.py
+++ src/detect_csv_url.py
@@ -1,13 +1,14 @@
-from url_content_type_services.url_content_type_services import ContentTypeReaderServiceInterface
+from url_content_type_services.url_content_type_services import IRequestService
 
 
-class CsvDective:
-    def __init__(self, content_type: ContentTypeReaderServiceInterface):
-        self.content_type = content_type
+def is_csv(url: str, requestService: IRequestService) -> bool:
 
-    def is_csv(self, url: str) -> bool:
-        content_type = self.content_type.get_content_type_from_url_header(url)
-        content_type_arr = content_type.split(";")
-        if content_type_arr[0] == "text/csv":
-            return True
+    content_type = requestService.get_content_type_from_header(url)
+    if content_type is None:
         return False
+
+    content_type_arr = content_type.split(";")
+    if content_type_arr[0] == "text/csv":
+        return True
+
+    return False
