29575b93479a114d1feeebe5450aa4b89b903b3b
sarrabah <sarrabenarbia1996@gmail.com>
2024-05-22 14:56:09 +0200

    refactor: Rename classes and add a new condition

--- src/url_content_type_services/url_content_type_services.py
+++ src/url_content_type_services/url_content_type_services.py
@@ -1,17 +1,18 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+from typing import Optional
 import requests
 
 
-class ContentTypeReaderServiceInterface(ABC):
-    @abstractmethod
-    def get_content_type_from_url_header(self, url: str) -> str:
+class IRequestService(ABC):
+    def get_content_type_from_header(self, url: str) -> Optional[str]:
         ...
 
 
-class ContentTypeReaderService(ContentTypeReaderServiceInterface):
-  
-    def get_content_type_from_url_header(self, url: str) -> str:
+class RequestService(IRequestService):
+
+    def get_content_type_from_url_header(self, url: str) -> Optional[str]:
         head = requests.head(url)
         header = head.headers
-        content_type = header["Content-type"]
-        return content_type
+        if "Content-Type" in header:
+            return header["Content-Type"]
+        return None
